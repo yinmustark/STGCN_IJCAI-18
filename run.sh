df2054bf4758db9108ca2698b460df1034b60885
yinmustark <dhcshig@163.com>
2020-05-22 17:48:36 +0800

    运行好了，保存一下

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-for rank_s in 10 20 30
+for rank_s in 3 15 25
 do
-    save_dir="./output$rank_s"
+    save_dir="./output_new$rank_s"
     mkdir -p $save_dir
     python main.py --rank $rank_s 3 --save_path $save_dir
 done
